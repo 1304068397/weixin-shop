9b09c131a620ad7a7b198171388daca038311ffe
wangzongbao1996 <wangzongbao@seaskysh.com>
2020-08-28 16:31:25 +0800

    提交版本 0.1

---
+++ shop-web/src/main/java/com/itunion/shop/web/controller/A001Ctrl.java
@@ -0,0 +1,18 @@
+package com.itunion.shop.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 王宗保
+ * @version V1.0
+ * @Package com.itunion.shop.web.controller
+ * @date 2020/8/28 16:28
+ * @Copyright © 2016-2017 海天信息工程有限公司
+ */
+@RestController
+@RequestMapping("a")
+public class A001Ctrl {
+
+}
